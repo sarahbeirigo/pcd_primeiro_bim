625a669432164762953b049f60577776371d96f7
Sarah Beirigo <sarahcbeirigo@gmail.com>
2025-09-21 18:55:41 -0300

    feat: Adiciona o modelo Rifa e Bilhete

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Rifa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    data_sorteio = models.DateTimeField()
+
+    def __str__(self):
+        return self.nome
+
+class Bilhete(models.Model):
+    rifa = models.ForeignKey(Rifa, on_delete=models.CASCADE, related_name='bilhetes')
+    numero = models.IntegerField()
+    comprador = models.CharField(max_length=100)
+    vendido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Bilhete {self.numero} da rifa {self.rifa.nome}'
