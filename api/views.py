c249c4f509e1294d2a800ddb2613d91731530720
Sarah Beirigo <sarahcbeirigo@gmail.com>
2025-09-21 19:01:30 -0300

    Revert "feat: Cria os endpoints da API para Rifa e Bilhete"
    This reverts commit a583f8bfa7d4e2e38092c277bdf49ef09fb52b20.

--- api/views.py
+++ api/views.py
@@ -1,11 +1,3 @@
-from rest_framework import viewsets
-from .models import Rifa, Bilhete
-from .serializers import RifaSerializer, BilheteSerializer
+from django.shortcuts import render
 
-class RifaViewSet(viewsets.ModelViewSet):
-    queryset = Rifa.objects.all()
-    serializer_class = RifaSerializer
-
-class BilheteViewSet(viewsets.ModelViewSet):
-    queryset = Bilhete.objects.all()
-    serializer_class = BilheteSerializer
+# Create your views here.
